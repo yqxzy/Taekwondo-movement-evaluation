4fb05123d5051c71f99bdd4ead66d8e012fff05d
Ooi-YeeJing <11910238@mail.sustech.edu.cn>
2022-05-03 22:13:53 +0800

    Update grade.py

--- tme/grade.py
+++ tme/grade.py
@@ -7,7 +7,7 @@ from partition import compute_difference
 def score(data):
     grade = {}
     frame = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-             19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
+             19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
     grade["action1"] = score_act1(frame[0:3], data, 0)
     grade["action2"] = score_act2(frame[3:5], data, 0)
     grade["action3"] = score_act3(frame[5:6], data, 0)
@@ -23,6 +23,11 @@ def score(data):
     grade["action13"] = score_act13(frame[22:23], data, 0)
     grade["action14"] = score_act14(frame[23:25], data, 0)
     grade["action15"] = score_act15(frame[25:29], data, 0)
+    grade["action16"] = score_act16(frame[29:31], data, 0)
+    grade["action17"] = score_act17(frame[31:35], data, 0)
+    grade["action18"] = score_act18(frame[35:37], data, 0)
+    grade["action19"] = score_act19(frame[37:38], data, 0)
+    grade["action20"] = score_act20(frame[38:], data, 0)
 
     return grade
 
@@ -480,6 +485,166 @@ def score_act15(frame, data, angle_list):
     feet_distance = abs(data[frame[3], 15, 0] - data[frame[3], 19, 0])
     if feet_distance > 0.3 or feet_distance < 0.2:
         m -= 0.1
+    m = ('%.1f' % m)
+    score_list.append(m)
+
+    return score_list
+
+
+def score_act16(frame, data, angle_list):
+    score_list = []
+
+    # 1. 脚不变，左手(右)肩部，右手(左)腰部
+    wrist_x, wrist_y = data[frame[0], 8, 0], (data[frame[0], 0, 0] + data[frame[0], 1, 0]) / 2 - 0.05
+    m = 0
+    # Left-hand on right-shoulder
+    if compute_difference(data[frame[0], 11, :], data[frame[0], 4, :]) > 0.1:
+        m = -0.1
+    # Right-hand on left-waist
+    if abs(data[frame[0], 7, 0] - wrist_x) > 0.05 or abs(data[frame[0], 7, 1] - wrist_y) > 0.1:
+        m -= 0.1
+    m = ('%.1f' % m)
+    score_list.append(m)
+
+    # 2. 左手(左)腰部，右拳角度，右脚向前一步
+    # Left-hand on left waist
+    waist_x, waist_y = data[frame[1], 8, 0], (data[frame[1], 0, 1] + data[frame[1], 1, 1]) / 2 - 0.05
+    m = 0
+    if abs(data[frame[1], 11, 0] - waist_x) > 0.05 or abs(data[frame[1], 11, 1] - waist_y) > 0.1:
+        m = - 0.1
+    # Feet distance (Right feet in front)
+    feet_distance = abs(data[frame[1], 15, 0] - data[frame[1], 19, 0])
+    if feet_distance > 0.3 or feet_distance < 0.2:
+        m -= 0.1
+    m = ('%.1f' % m)
+    score_list.append(m)
+
+    return score_list
+
+
+def score_act17(frame, data, angle_list):
+    score_list = []
+
+    # 1. 双手在胸口到腰之间，左腿角度（上提），右腿角度（直）
+    m = 0
+    # Both hand between chest and waist
+    waist_y = (data[frame[0], 0, 1] + data[frame[0], 1, 1]) / 2
+    upper_waist_y = (data[frame[0], 0, 1] + waist_y) / 2 - 0.05
+    if (abs(data[frame[0], 7, 1] - upper_waist_y)) > 0.1 or (
+            abs(data[frame[0], 11, 1] - upper_waist_y) > 0.1):
+        m = -0.1
+    m = ('%.1f' % m)
+    score_list.append(m)
+
+    # 2. 双手不变，双腿角度(直)，左腿高于肩膀
+    # Left feet higher than shoulder
+    m = 0
+    if data[frame[1], 19, 1] < data[frame[1], 8, 1]:
+        m -= 0.1
+    m = ('%.1f' % m)
+    score_list.append(m)
+
+    # 3. 左手(左)腰部，右反手冲拳位置及角度，左腿角度（上提），右腿角度（直）
+    # Left-hand on left-waist
+    waist_x, waist_y = data[frame[2], 8, 0], (data[frame[2], 0, 1] + data[frame[2], 1, 1]) / 2 - 0.05
+    m = 0
+    if abs(data[frame[2], 11, 0] - waist_x) > 0.05 or abs(data[frame[2], 11, 1] - waist_y) > 0.1:
+        m = - 0.1
+    # Right-hand punch
+    if abs(data[frame[2], 7, 1] - data[frame[2], 1, 1]) > 0.1 or abs(
+            data[frame[2], 7, 0] - data[frame[2], 1, 0]) > 0.1:
+        m -= 0.1
+    m = ('%.1f' % m)
+    score_list.append(m)
+
+    # 4. 右手(右)腰部，左反手冲拳, 左脚向前一步
+    # Right-hand on right-waist
+    waist_x, waist_y = data[frame[3], 4, 0], (data[frame[3], 0, 1] + data[frame[3], 1, 1]) / 2 - 0.05
+    m = 0
+    if abs(data[frame[3], 7, 0] - waist_x) > 0.05 or abs(data[frame[3], 7, 1] - waist_y) > 0.1:
+        m = - 0.1
+    # Left-hand punch
+    if abs(data[frame[3], 11, 1] - data[frame[3], 1, 1]) > 0.1 or abs(
+            data[frame[3], 11, 0] - data[frame[3], 1, 0]) > 0.1:
+        m -= 0.1
+    # Feet distance (Left feet in front)
+    feet_distance = abs(data[frame[3], 19, 0] - data[frame[3], 15, 0])
+    if feet_distance > 0.3 or feet_distance < 0.2:
+        m -= 0.1
+    m = ('%.1f' % m)
+    score_list.append(m)
+
+    return score_list
+
+
+def score_act18(frame, data, angle_list):
+    score_list = []
+
+    # 1. 左手(右)肩部，右手(左)腰部
+    wrist_x, wrist_y = data[frame[0], 8, 0], (data[frame[0], 0, 0] + data[frame[0], 1, 0]) / 2 - 0.05
+    m = 0
+    # Left-hand on right-shoulder
+    if compute_difference(data[frame[0], 11, :], data[frame[0], 4, :]) > 0.1:
+        m = -0.1
+    # Right-hand on left-waist
+    if abs(data[frame[0], 7, 0] - wrist_x) > 0.05 or abs(data[frame[0], 7, 1] - wrist_y) > 0.1:
+        m -= 0.1
+    m = ('%.1f' % m)
+    score_list.append(m)
+
+    # 2. 右手(右)腰部，左拳角度，双腿角度
+    # Right-hand on right waist
+    waist_x, waist_y = data[frame[1], 4, 0], (data[frame[1], 0, 1] + data[frame[1], 1, 1]) / 2 - 0.05
+    m = 0
+    if abs(data[frame[1], 7, 0] - waist_x) > 0.05 or abs(data[frame[1], 7, 1] - waist_y) > 0.1:
+        m = - 0.1
+    m = ('%.1f' % m)
+    score_list.append(m)
+
+    return score_list
+
+
+def score_act19(frame, data, angle_list):
+    score_list = []
+
+    # 1. 左手(左)腰部，右反手冲拳，双腿角度
+    # Left-hand on left-waist
+    waist_x, waist_y = data[frame[0], 8, 2], (data[frame[0], 0, 1] + data[frame[0], 1, 1]) / 2 - 0.05
+    m = 0
+    if abs(data[frame[0], 11, 2] - waist_x) > 0.05 or abs(data[frame[0], 11, 1] - waist_y) > 0.1:
+        m = - 0.1
+    # Right-hand punch
+    if abs(data[frame[0], 7, 1] - data[frame[0], 1, 1]) > 0.1 or abs(
+            data[frame[0], 7, 2] - data[frame[0], 1, 2]) > 0.1:
+        m -= 0.1
+    m = ('%.1f' % m)
+    score_list.append(m)
+
+    return score_list
+
+
+def score_act20(frame, data, angle_list):
+    score_list = []
+
+    # 1. 提至胸口握拳，拳心向上: 双手在胸口，肩膀角度，两脚距离
+    m = 0
+    if (abs(data[frame[0], 7, 1] - data[frame[0], 1, 1])) > 0.1 or (
+            abs(data[frame[0], 11, 1] - data[frame[0], 1, 1]) > 0.1):
+        m = -0.1
+    if abs(data[frame[0], 15, 0] - data[frame[0], 19, 0]) < 0.3 or abs(
+            data[frame[0], 15, 0] - data[frame[0], 19, 0]) > 0.5:
+        m = m - 0.1
+    m = ('%.1f' % m)
+    score_list.append(m)
+
+    # 2. 双手小腹前，两拳之间一拳距离
+    m = 0
+    if (data[frame[1], 7, 1] > data[frame[1], 0, 1] + 0.2 or data[frame[1], 7, 1] < data[frame[1], 0, 1] + 0.1) or (
+            data[frame[1], 11, 1] > data[frame[1], 1, 1] + 0.2 or data[frame[1], 11, 1] < data[frame[1], 1, 1] + 0.1):
+        m = -0.1
+    if abs(data[frame[1], 7, 0] - data[frame[1], 11, 0]) < 0.1 or abs(
+            data[frame[1], 7, 0] - data[frame[1], 11, 0]) > 0.15:
+        m = m - 0.1
     m = ('%.1f' % m)
     score_list.append(m)
 
