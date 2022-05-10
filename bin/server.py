d3d4c64e809b8e7efe58dbd541d9a39cac00e563
magicalMaggie <1257577635@qq.com>
2022-05-10 21:05:00 +0800

    update

--- bin/server.py
+++ bin/server.py
@@ -1,4 +1,5 @@
 import ctypes
+import struct
 import sys
 import time
 from socket import *
@@ -51,7 +52,7 @@ class KinectServer(object):
         self.pic = False
         self.bone_file_2 = None
         self.bone_file_1 = None
-        self.data = None
+        self.data = "".encode()
         self.conn = None
         self.addr = None
         self.video = None
@@ -79,104 +80,84 @@ class KinectServer(object):
 
     def draw_body(self, joints, jointPoints, color):
         # save position
+        payload_size = struct.calcsize("Q")
+        self.data = b''
+        while len(self.data) < payload_size:
+            self.data += self.conn.recv(1024)
+        print(len(self.data))
+        packed_size = self.data[:payload_size]
+        self.data = self.data[payload_size:]
+        msg_size = struct.unpack("Q", packed_size)[0]
+        print(msg_size)
+
+        # while len(self.data) < msg_size:
+        #     self.data += self.conn.recv(1024)
+        zframe_data = self.data[:msg_size]
+        self.data = zframe_data
+        # print(self.data)
+
         data_1 = ""
         if self.start:
             self._text_file_2.write(self.data.decode())
-            data_1 += (
-                "%f,%f,%f,%d\n" % (
-                    joints[0].Position.x, joints[0].Position.y, joints[0].Position.z, joints[0].TrackingState))
-            data_1 += (
-                "%f,%f,%f,%d\n" % (
-                    joints[1].Position.x, joints[1].Position.y, joints[1].Position.z, joints[1].TrackingState))
-            data_1 += (
-                "%f,%f,%f,%d\n" % (
-                    joints[2].Position.x, joints[2].Position.y, joints[2].Position.z, joints[2].TrackingState))
-            data_1 += (
-                "%f,%f,%f,%d\n" % (
-                    joints[3].Position.x, joints[3].Position.y, joints[3].Position.z, joints[3].TrackingState))
-            data_1 += (
-                "%f,%f,%f,%d\n" % (
-                    joints[4].Position.x, joints[4].Position.y, joints[4].Position.z, joints[4].TrackingState))
-            data_1 += (
-                "%f,%f,%f,%d\n" % (
-                    joints[5].Position.x, joints[5].Position.y, joints[5].Position.z, joints[5].TrackingState))
-            data_1 += (
-                "%f,%f,%f,%d\n" % (
-                    joints[6].Position.x, joints[6].Position.y, joints[6].Position.z, joints[6].TrackingState))
-            data_1 += (
-                "%f,%f,%f,%d\n" % (
-                    joints[7].Position.x, joints[7].Position.y, joints[7].Position.z, joints[7].TrackingState))
-            data_1 += (
-                "%f,%f,%f,%d\n" % (
-                    joints[8].Position.x, joints[8].Position.y, joints[8].Position.z, joints[8].TrackingState))
-            data_1 += (
-                "%f,%f,%f,%d\n" % (
-                    joints[9].Position.x, joints[9].Position.y, joints[9].Position.z, joints[9].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[10].Position.x, joints[10].Position.y, joints[10].Position.z, joints[10].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[11].Position.x, joints[11].Position.y, joints[11].Position.z, joints[11].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[12].Position.x, joints[12].Position.y, joints[12].Position.z, joints[12].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[13].Position.x, joints[13].Position.y, joints[13].Position.z, joints[13].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[14].Position.x, joints[14].Position.y, joints[14].Position.z, joints[14].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[15].Position.x, joints[15].Position.y, joints[15].Position.z, joints[15].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[16].Position.x, joints[16].Position.y, joints[16].Position.z, joints[16].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[17].Position.x, joints[17].Position.y, joints[17].Position.z, joints[17].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[18].Position.x, joints[18].Position.y, joints[18].Position.z, joints[18].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[19].Position.x, joints[19].Position.y, joints[19].Position.z, joints[19].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[20].Position.x, joints[20].Position.y, joints[20].Position.z, joints[20].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[21].Position.x, joints[21].Position.y, joints[21].Position.z, joints[21].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[22].Position.x, joints[22].Position.y, joints[22].Position.z, joints[22].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[23].Position.x, joints[23].Position.y, joints[23].Position.z, joints[23].TrackingState))
-            data_1 += ("%f,%f,%f,%d\n" % (
-                joints[24].Position.x, joints[24].Position.y, joints[24].Position.z, joints[24].TrackingState))
+
+            for i in range(25):
+                data_1 += (
+                        "%f,%f,%f,%d\n" % (
+                    joints[i].Position.x, joints[i].Position.y, joints[i].Position.z, joints[i].TrackingState))
+
             self._text_file_1.write(data_1)
+
         if self.pic:
+            for i in range(25):
+                data_1 += (
+                        "%f,%f,%f,%d\n" % (
+                    joints[i].Position.x, joints[i].Position.y, joints[i].Position.z, joints[i].TrackingState))
+
             self.bone_file_1.write(data_1)
-            self.bone_file_2.write(self.data.decode)
+            self.bone_file_2.write(self.data.decode())
             self.bone_file_1.close()
             self.bone_file_2.close()
             self.pic = False
         # Torso
         self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_Head, PyKinectV2.JointType_Neck);
         self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_Neck, PyKinectV2.JointType_SpineShoulder);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineShoulder, PyKinectV2.JointType_SpineMid);
+        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineShoulder,
+                            PyKinectV2.JointType_SpineMid);
         self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineMid, PyKinectV2.JointType_SpineBase);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineShoulder, PyKinectV2.JointType_ShoulderRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineShoulder,PyKinectV2.JointType_ShoulderLeft);
+        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineShoulder,
+                            PyKinectV2.JointType_ShoulderRight);
+        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineShoulder,
+                            PyKinectV2.JointType_ShoulderLeft);
         self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineBase, PyKinectV2.JointType_HipRight);
         self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_SpineBase, PyKinectV2.JointType_HipLeft);
 
         # Right Arm
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ShoulderRight, PyKinectV2.JointType_ElbowRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ElbowRight, PyKinectV2.JointType_WristRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristRight, PyKinectV2.JointType_HandRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_HandRight, PyKinectV2.JointType_HandTipRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristRight, PyKinectV2.JointType_ThumbRight);
+        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ShoulderRight,
+                            PyKinectV2.JointType_ElbowRight);
+        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ElbowRight,
+                            PyKinectV2.JointType_WristRight);
+        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristRight,
+                            PyKinectV2.JointType_HandRight);
+        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_HandRight,
+                            PyKinectV2.JointType_HandTipRight);
+        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristRight,
+                            PyKinectV2.JointType_ThumbRight);
 
         # Left Arm
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ShoulderLeft, PyKinectV2.JointType_ElbowLeft);
+        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ShoulderLeft,
+                            PyKinectV2.JointType_ElbowLeft);
         self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_ElbowLeft, PyKinectV2.JointType_WristLeft);
         self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristLeft, PyKinectV2.JointType_HandLeft);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_HandLeft, PyKinectV2.JointType_HandTipLeft);
+        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_HandLeft,
+                            PyKinectV2.JointType_HandTipLeft);
         self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_WristLeft, PyKinectV2.JointType_ThumbLeft);
 
         # Right Leg
         self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_HipRight, PyKinectV2.JointType_KneeRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_KneeRight, PyKinectV2.JointType_AnkleRight);
-        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_AnkleRight, PyKinectV2.JointType_FootRight);
+        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_KneeRight,
+                            PyKinectV2.JointType_AnkleRight);
+        self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_AnkleRight,
+                            PyKinectV2.JointType_FootRight);
 
         # Left Leg
         self.draw_body_bone(joints, jointPoints, color, PyKinectV2.JointType_HipLeft, PyKinectV2.JointType_KneeLeft);
@@ -204,16 +185,16 @@ class KinectServer(object):
                 continue
             # -------- Main Program Loop -----------
             while not self._done:
+                # --- Game logic should go here
                 # --- Main event loop
-                self.data = self.conn.recv(1024)
-
                 for event in pygame.event.get():  # User did something
                     if event.type == pygame.QUIT:  # If user clicked close
                         self._done = True  # Flag that we are done so we exit this loop
 
                     elif event.type == pygame.VIDEORESIZE:  # window resized
                         self._screen = pygame.display.set_mode(event.dict['size'],
-                                                               pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE, 32)
+                                                               pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE,
+                                                               32)
                     elif event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_RETURN:  # 按回車開始
                             print("Start!")
@@ -235,15 +216,12 @@ class KinectServer(object):
                         if event.key == pygame.K_p:  # 按P拍照
                             pic_name = "pic_%d.png" % self.pic_num
                             bone_1 = "Snaps/pic_%d_1.txt" % self.pic_num
-                            bone_2 = "Snaps/8pic_%d_2.txt" % self.pic_num
+                            bone_2 = "Snaps/pic_%d_2.txt" % self.pic_num
                             pygame.image.save(self._screen, pic_name)
                             self.bone_file_1 = open(bone_1, mode='w')
                             self.bone_file_2 = open(bone_2, mode='w')
                             self.pic_num = self.pic_num + 1
                             self.pic = True
-
-                # --- Game logic should go here
-
                 # --- Getting frames and drawing
                 # --- Woohoo! We've got a color frame! Let's fill out back buffer surface with frame's data
                 if self._kinect.has_new_color_frame():
@@ -257,21 +235,22 @@ class KinectServer(object):
 
                 # --- draw skeletons to _frame_surface
                 if self._bodies is not None:
-                    for i in range(0, self._kinect.max_body_count):
-                        body = self._bodies.bodies[i]
-                        if not body.is_tracked:
-                            continue
+                    # for i in range(0, self._kinect.max_body_count):
+                    body = self._bodies.bodies[0]
+                    if not body.is_tracked:
+                        continue
 
-                        joints = body.joints
-                        # convert joint coordinates to color space
-                        joint_points = self._kinect.body_joints_to_color_space(joints)
-                        self.draw_body(joints, joint_points, SKELETON_COLORS[i])
+                    joints = body.joints
+                    # convert joint coordinates to color space
+                    joint_points = self._kinect.body_joints_to_color_space(joints)
+                    self.draw_body(joints, joint_points, SKELETON_COLORS[0])
 
                 # --- copy back buffer surface pixels to the screen, resize it if needed and keep aspect ratio
                 # --- (screen size may be different from Kinect's color frame size)
                 h_to_w = float(self._frame_surface.get_height()) / self._frame_surface.get_width()
                 target_height = int(h_to_w * self._screen.get_width())
-                surface_to_draw = pygame.transform.scale(self._frame_surface, (self._screen.get_width(), target_height));
+                surface_to_draw = pygame.transform.scale(self._frame_surface,
+                                                         (self._screen.get_width(), target_height));
                 self._screen.blit(surface_to_draw, (0, 0))
                 surface_to_draw = None
                 pygame.display.update()
